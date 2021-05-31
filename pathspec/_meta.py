35e5e19febc1af3a7752a019f85c7149e3c7603d
cpburnz <cpburnz@gmail.com>
2021-05-31 00:07:53 -0400

    Move metadata out from __init__.py

---
+++ pathspec/_meta.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+"""
+This module contains the project meta-data.
+"""
+
+__author__ = "Caleb P. Burns"
+__copyright__ = "Copyright © 2013-2021 Caleb P. Burns"
+__credits__ = [
+	"dahlia <https://github.com/dahlia>",
+	"highb <https://github.com/highb>",
+	"029xue <https://github.com/029xue>",
+	"mikexstudios <https://github.com/mikexstudios>",
+	"nhumrich <https://github.com/nhumrich>",
+	"davidfraser <https://github.com/davidfraser>",
+	"demurgos <https://github.com/demurgos>",
+	"ghickman <https://github.com/ghickman>",
+	"nvie <https://github.com/nvie>",
+	"adrienverge <https://github.com/adrienverge>",
+	"AndersBlomdell <https://github.com/AndersBlomdell>",
+	"highb <https://github.com/highb>",
+	"thmxv <https://github.com/thmxv>",
+	"wimglenn <https://github.com/wimglenn>",
+	"hugovk <https://github.com/hugovk>",
+	"dcecile <https://github.com/dcecile>",
+	"mroutis <https://github.com/mroutis>",
+	"jdufresne <https://github.com/jdufresne>",
+	"groodt <https://github.com/groodt>",
+	"ftrofin <https://github.com/ftrofin>",
+	"pykong <https://github.com/pykong>",
+	"nhhollander <https://github.com/nhhollander>",
+	"KOLANICH <https://github.com/KOLANICH>"
+]
+__license__ = "MPL 2.0"
+__version__ = "0.8.1"
---
+++ pathspec_meta.py
@@ -0,0 +1,1 @@
+pathspec/_meta.py
